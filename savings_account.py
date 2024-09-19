3c05581b993c95fe18c826dca9422d7e7019fd9d
jbostok1 <jbostok@charlotte.edu>
2024-09-19 13:51:48 -0400

    Added interest function

--- savings_account.py
+++ savings_account.py
@@ -5,3 +5,8 @@ class SavingsAccount(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, account_number,routing_number, interest_rate):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.interest_rate = interest_rate
+
+    def add_interest(self):
+        interest = self.current_balance * self.interest_rate
+        self.current_balance += interest
+        print(f"\nInterest: ${interest:.2f} | New Balance: ${self.current_balance:.2f}")
